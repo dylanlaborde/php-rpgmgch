7e653b9f09970a06264c4fc6281efba277d3bab3
Dylan <dylan4047@gmail.com>
2017-01-23 11:27:36 +0100

    ajout d'un constructor

--- Models/Client.php
+++ Models/Client.php
@@ -4,6 +4,14 @@ require 'User.php';
 
 class Client extends User{
 
+public function  __construct($id,$email){
+		$this->setEmail($email);
+		$this->setId($id);
+		
+	}
+
+
+
 	public function buy(){
 
 
