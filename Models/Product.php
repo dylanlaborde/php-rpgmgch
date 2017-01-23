6c1a75b20c3b8c872612be3135070f94fc86b0e0
Dylan <dylan4047@gmail.com>
2017-01-23 14:05:09 +0100

    ajout de la class Product et des getter/setter

--- Models/Product.php
+++ Models/Product.php
@@ -1,8 +1,33 @@
-<?php 	
+<?php 
+class Product{
 
+	private $id;
+	private $name;
+	private $price;
+/*
+Getter
+*/
+	public function getId(){
+		return $this->id;
+	}
+	public function getPrice(){
+		return $this->price;
+	}
+	public function getName(){
+		return $this->name;
+	}
+/*
+Setter
+*/
+	public function setPrice($price){
+		$this->price = $price;
+	}
+	public function setName($name){
+		$this->name = $name;
+	}
+	public function setId($id){
+		$this->id = $id;
+	}
 
-
-
-
-
- ?>
+}
+?>
