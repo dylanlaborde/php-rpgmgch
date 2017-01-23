c2b7e85f71d869872bba082264e42f08e8c36ccd
Dylan <dylan4047@gmail.com>
2017-01-23 15:51:50 +0100

    creation de la class vegetable initialisation des variable getter et setter et creation de la function isFresh() qui return true/false pour savoir si le produit peut etre consomée

--- Models/Vegetable.php
+++ Models/Vegetable.php
@@ -1,6 +1,34 @@
-<?php 
+<?php
 
+require 'Product.php';
 
+class Vegetable extends Product{
+	private $productorName;
+	private $expiresAt;
 
+	public function setExpiresAt($expiresAt){
+		$this->expiresAt =new DateTime($expiresAt);
+	}
+	public function getExpiresAt(){
+		return $this->expiresAt;
+	}
 
- ?>
+	public function isFresh(){
+		print_r($this->actualTime);
+		print_r($this->expiresAt);
+
+		if ($this->actualTime > $this->expiresAt) {
+			return false;
+		}
+		if ($this->actualTime < $this->expiresAt) {
+			return true;
+		}
+
+	}
+	public function __construct(){
+		$this->actualTime= new DateTime();
+		
+	}
+
+}
+?>
