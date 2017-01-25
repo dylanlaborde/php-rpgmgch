13709d643d0e5929b1fc734417dcae2ade740151
Dylan <dylan4047@gmail.com>
2017-01-25 02:28:58 +0100

    rajout __construct

--- Models/Cloth.php
+++ Models/Cloth.php
@@ -6,6 +6,12 @@ class Cloth extends Product{
 
 	private $brand;
 
+	public function __construct($id,$name,$brand,$price){
+		$this->setId($id);
+		$this->setName($name);
+		$this->setBrand($brand);
+		$this->setPrice($price);
+	}
 	public function getBrand(){
 		return $this->brand;
 	}
@@ -16,6 +22,5 @@ class Cloth extends Product{
 	public function try(){
 
 	}
-
 }
  ?>
