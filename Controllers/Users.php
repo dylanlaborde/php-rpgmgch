a0b11c366369ae4b8203b5b110cc09f2805dcfe1
Dylan <dylan4047@gmail.com>
2017-01-25 02:27:05 +0100

    modif chemin require

--- Controllers/Users.php
+++ Controllers/Users.php
@@ -1,6 +1,6 @@
 <?php 
 
-require __DIR__."/../Client.php";
+require __DIR__."/../Models/Client.php";
 
 $client1 = new Client("user1","user1@gmail.com");
 $client2 = new Client("user2","user2@gmail.com");
@@ -8,7 +8,6 @@ return [
   ["client"=>$client1],
   ["client"=>$client2]
 ];
-
 
 ?>
 <!-- date("F j, Y, g:i a"); -->
