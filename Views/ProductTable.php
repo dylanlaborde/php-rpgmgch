790ae927c5d106f9439bd4d478f8daf76b7dbbfa
Dylan <dylan4047@gmail.com>
2017-01-25 08:58:52 +0100

    ajout de l'affichage des tableau produit et vetement

--- Views/ProductTable.php
+++ Views/ProductTable.php
@@ -1,3 +1,6 @@
+<?php 
+require __DIR__."/../Controllers/Products.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +8,45 @@
 	<title>liste de produit</title>
 </head>
 <body>
-	 <h2>Liste des produit</h2>
+	<h2>Liste des produit</h2>
+
+
+	<?php foreach($Products as $Product): ?>
+		<table>
+			<tr>
+				<th>id</th>
+				<th>name</th>
+				<th>price</th>
+			</tr>
+			<tr>
+				<td><?= $Product['legume']->getId() ?></td>
+				<td><?= $Product['legume']->getName()?></td>
+				<td><?=$Product['legume']->getPrice() ?></td>
+			</tr>
+		</table>
+	<?php endforeach ?>
+	<?php foreach ($Vetements as $vetement):?>
+		<table>
+			<tr>
+				<th>id</th>
+				<th>name</th>
+				<th>price</th>
+				<th>brand</th>
+			</tr>
+			<tr>
+
+				<td><?= $vetement['vetement']->getId() ?></td>
+				<td><?= $vetement['vetement']->getName()?></td>
+				<td><?=$vetement['vetement']->getPrice() ?></td>
+				<td><?=$vetement['vetement']->getBrand() ?></td>
+			</tr>
+		</table>
+	<?php endforeach ?>
+
+
+
+
+
+
 </body>
 </html>
